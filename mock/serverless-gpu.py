b1a02c79c73c292671976058a9bf446623d5e74d
Jingyi Zhao <zhao.elton@gmail.com>
2025-04-21 00:37:05 -0700

    - mock data

---
+++ mock/serverless-gpu.py
@@ -0,0 +1,97 @@
+prices = [
+    {
+        "gpu_model": "NVIDIA H100 SXM",
+        "vram(GB)": "80",
+        "price($)": 2.99,
+    },
+    {
+        "gpu_model": "NVIDIA H100 SXM",
+        "vram(GB)": "80",
+        "price($)": "NaN",
+    },
+    {
+        "gpu_model": "NVIDIA A100 SXM",
+        "vram(GB)": "80",
+        "price($)": 1.79,
+    },
+    {
+        "gpu_model": "NVIDIA A100 SXM",
+        "vram(GB)": "40",
+        "price($)": 1.29,
+    },
+    {
+        "gpu_model": "NVIDIA Tesla V100",
+        "vram(GB)": "16",
+        "price($)": 0.55,
+    },
+    {
+        "gpu_model": "NVIDIA H100 SXM",
+        "vram(GB)": "80",
+        "price($)": 3.09,
+    },
+    {
+        "gpu_model": "NVIDIA A100 PCIe",
+        "vram(GB)": "40",
+        "price($)": 1.29,
+    },
+    {
+        "gpu_model": "NVIDIA A6000",
+        "vram(GB)": "48",
+        "price($)": 0.8,
+    },
+    {
+        "gpu_model": "NVIDIA H100 SXM",
+        "vram(GB)": "80",
+        "price($)": 3.19,
+    },
+    {
+        "gpu_model": "NVIDIA A100 PCIe",
+        "vram(GB)": "40",
+        "price($)": 1.29,
+    },
+    {
+        "gpu_model": "NVIDIA A6000",
+        "vram(GB)": "48",
+        "price($)": 0.8,
+    },
+    {
+        "gpu_model": "NVIDIA GH200",
+        "vram(GB)": "96",
+        "price($)": 1.49,
+    },
+    {
+        "gpu_model": "NVIDIA H100 SXM",
+        "vram(GB)": "80",
+        "price($)": 3.29,
+    },
+    {
+        "gpu_model": "NVIDIA H100 PCIe",
+        "vram(GB)": "80",
+        "price($)": 2.49,
+    },
+    {
+        "gpu_model": "NVIDIA A100 SXM",
+        "vram(GB)": "40",
+        "price($)": 1.29,
+    },
+    {
+        "gpu_model": "NVIDIA A100 PCIe",
+        "vram(GB)": "40",
+        "price($)": 1.29,
+    },
+    {
+        "gpu_model": "NVIDIA A10",
+        "vram(GB)": "24",
+        "price($)": 0.75,
+    },
+    {
+        "gpu_model": "NVIDIA A6000",
+        "vram(GB)": "48",
+        "price($)": 0.8,
+    },
+    {
+        "gpu_model": "NVIDIA Quadro RTX 6000",
+        "vram(GB)": "24",
+        "price($)": 0.5,
+    }
+]
